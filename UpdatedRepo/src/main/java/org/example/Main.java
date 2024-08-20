7cec8ffa7983240592eb798d95edd4752dca59d1
NadaMahmoud16 <missnadamahmoud@gmail.com>
2024-08-20 20:45:59 +0300

    This is a brief introduction of myself

--- UpdatedRepo/src/main/java/org/example/Main.java
+++ UpdatedRepo/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("My name is Nada, I'm 30 years old, I'm working as software Quality Assurance Tester.");
     }
 }
